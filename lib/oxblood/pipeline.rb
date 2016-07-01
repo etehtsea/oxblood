bcb09a8578bd565ed2c284c4ef8374c7482b1687
Konstantin Shabanov <etehtsea@gmail.com>
2016-07-01 23:13:28 +0700

    Fix Pipeline class and add basic docs and specs

--- lib/oxblood/pipeline.rb
+++ lib/oxblood/pipeline.rb
@@ -1,18 +1,33 @@
+require 'oxblood/session'
+
 module Oxblood
+  # Redis pipeling class. Commands won't be send until {#sync} is called.
+  # @see http://redis.io/topics/pipelining#redis-pipelining
+  #
+  # @example Basic workflow
+  #   pipeline = Pipeline.new(connection)
+  #   pipeline.echo('ping')
+  #   pipeline.ping
+  #   pipeline.echo('!')
+  #   pipeline.sync # => ["ping", "PONG", "!"]
   class Pipeline < Session
     def initialize(connection)
       super
       @commands = Array.new
     end
 
-    def run(command)
-      @commands << command
-    end
-
+    # Sends all commands at once and reads responses
+    # @return [Array] of responses
     def sync
-      serialized_commands = @commands.map { |c| serialize(command) }
+      serialized_commands = @commands.map { |c| serialize(*c) }
       @connection.write(serialized_commands.join)
       @connection.read_responses(@commands.size)
+    end
+
+    private
+
+    def run(*command)
+      @commands << command
     end
   end
 end
---
+++ spec/oxblood/pipeline_spec.rb
@@ -0,0 +1,19 @@
+require 'oxblood/pipeline'
+require 'oxblood/connection'
+
+RSpec.describe Oxblood::Pipeline do
+  describe '#sync' do
+    let(:connection) do
+      Oxblood::Connection.open
+    end
+
+    subject do
+      described_class.new(connection)
+    end
+
+    specify do
+      3.times { subject.echo 'hello' }
+      expect(subject.sync).to match_array(Array.new(3) { 'hello' })
+    end
+  end
+end
