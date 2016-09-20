4c4f8ef315e6e3cd25123d9faa912ed4acfc3686
Konstantin Shabanov <mail@etehtsea.me>
2016-09-20 23:50:55 +0700

    Extend pipelining benchmark

--- benchmarks/pipeline.rb
+++ benchmarks/pipeline.rb
@@ -1,3 +1,4 @@
+require 'hiredis' unless RUBY_ENGINE == 'jruby'
 require 'redis'
 require 'oxblood'
 require 'benchmark'
@@ -19,6 +20,16 @@ def redis_with
   r.pipelined { N.times { r.ping } }
 end
 
+def hiredis_without
+  r = Redis.new(driver: :hiredis)
+  N.times { r.ping }
+end
+
+def hiredis_with
+  r = Redis.new(driver: :hiredis)
+  r.pipelined { N.times { r.ping } }
+end
+
 def oxblood_without
   r = Oxblood::Session.new(Oxblood::Connection.new)
   N.times { r.ping }
@@ -30,7 +41,19 @@ def oxblood_with
   pipe.sync
 end
 
+# Warmup JVM
+if RUBY_ENGINE == 'jruby'
+  redis_without
+  redis_with
+  oxblood_without
+  oxblood_with
+end
+
 benchmark('redis without') { redis_without }
 benchmark('redis with') { redis_with }
 benchmark('oxblood without') { oxblood_without }
 benchmark('oxblood with') { oxblood_with }
+unless RUBY_ENGINE == 'jruby'
+  benchmark('hiredis without') { hiredis_without }
+  benchmark('hiredis with') { hiredis_with }
+end
