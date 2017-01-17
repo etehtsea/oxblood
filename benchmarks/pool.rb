110503fdcb3b15fd92d6573e58078b035fa79e20
Konstantin Shabanov <mail@etehtsea.me>
2017-01-17 12:01:51 +0700

    Fix pooling benchmark

--- benchmarks/pool.rb
+++ benchmarks/pool.rb
@@ -3,12 +3,14 @@ require 'redis'
 require 'oxblood'
 require 'benchmark'
 
-N = 10_000
-TASKS = 100
-POOL_SIZE = Concurrent.processor_count
+N = 5_000
+TASKS = 1_000
+POOL_SIZE = Concurrent.processor_count + 1
+
+Concurrent.use_stdlib_logger(Logger::DEBUG)
 
 def worker_pool
-  Concurrent::FixedThreadPool.new(POOL_SIZE * 2)
+  Concurrent::FixedThreadPool.new(POOL_SIZE)
 end
 
 RedisPool = ConnectionPool.new(size: POOL_SIZE) { Redis.new }
@@ -19,18 +21,31 @@ def benchmark(label, &blk)
   puts [label, sec.round(3)].join(': ')
 end
 
-def run(&blk)
+def run(name, &blk)
+  puts "Running #{name}"
+
   pool = worker_pool
   TASKS.times { pool.post(&blk) }
-  sleep 0.1 while pool.completed_task_count != TASKS
+  sleep 0.1 while pool.scheduled_task_count != TASKS
+  pool.shutdown
+  pool.wait_for_termination
+  puts "#{pool.completed_task_count} tasks finished successfully"
 end
 
 def redis
-  RedisPool.with { |r| r.pipelined { N.times { r.ping } } }
+  RedisPool.with do |r|
+    r.pipelined do
+      N.times { r.ping }
+    end
+  end
 end
 
 def oxblood
-  OxbloodPool.with { |s| s.pipelined { |p| N.times { p.ping } } }
+  OxbloodPool.with do |s|
+    s.pipelined do |p|
+      N.times { p.ping }
+    end
+  end
 end
 
 # Check that everything is working
@@ -45,5 +60,5 @@ if RUBY_ENGINE == 'jruby'
   end
 end
 
-benchmark('redis-rb') { run { redis } }
-benchmark('oxblood') { run { oxblood } }
+benchmark('redis-rb') { run('redis-rb') { redis } }
+benchmark('oxblood') { run('oxblood') { oxblood } }
