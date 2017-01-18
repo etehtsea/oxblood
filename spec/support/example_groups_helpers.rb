30068a43eafb766e80b84061f02e06c221f59c0f
Konstantin Shabanov <mail@etehtsea.me>
2017-01-18 16:25:54 +0700

    Supported version check should be inclusive

--- spec/support/example_groups_helpers.rb
+++ spec/support/example_groups_helpers.rb
@@ -8,6 +8,6 @@ module ExampleGroupsHelpers
     server_info_hash = Hash[server_info.split("\r\n")[1..-1].map { |e| e.split(':') }]
     server_version = Gem::Version.new(server_info_hash['redis_version'])
 
-    server_version > Gem::Version.new(version)
+    server_version >= Gem::Version.new(version)
   end
 end
